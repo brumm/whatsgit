38aa2eb181909ef790372250dcd08753fc1d68d6
Philipp Brumm <philipp@funkensturm.de>
2016-04-22 16:51:45 +0200

    change css-module identifier

--- nwb.config.js
+++ nwb.config.js
@@ -1,12 +1,11 @@
 const cssConfig = {
   query: {
     modules: true,
-    localIdentName: '[path]-[local]-[hash:base64:10]'
+    localIdentName: '[local]-[hash:base64:10]'
   }
 }
 
 module.exports = {
-  // Let nwb know this is a React app when generic build commands are used
   type: 'react-app',
 
   loaders: {
