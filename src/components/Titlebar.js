f721b9ce6f570da70e91644c8956fb77f6527fc5
Philipp Brumm <philipp@funkensturm.de>
2016-05-12 17:11:49 +0200

    some nicely styled refresh/logout buttons

--- src/components/Titlebar.js
+++ src/components/Titlebar.js
@@ -3,16 +3,27 @@ import Flex from 'flex-component'
 import { connect } from 'react-redux'
 import Octicon from 'react-octicon'
 
-import { refreshIssues } from '../redux/actions'
+import { refreshData } from '../redux/actions'
 import style from '../styles/Titlebar.scss'
 
-@connect(({ loggedIn }) => ({ loggedIn }))
+@connect(({ loggedIn }) => {
+  return {
+    loggedIn
+  }
+})
 export default class Titlebar extends React.Component {
   render() {
     return (
-      <Flex alignItems='center' justifyContent='flex-end' shrink={0} className={style.Titlebar}>
+      <Flex alignItems='center' shrink={0} className={style.Titlebar}>
         {this.props.loggedIn &&
-          <Octicon name="sync" onClick={() => this.props.dispatch(refreshIssues())} />
+          <Flex className={style.buttonGroup} style={{ marginLeft: 'auto' }}>
+            <button className={style.button} onClick={() => this.props.dispatch(refreshData())}>
+              <Octicon className={style.icon} name="sync" />
+            </button>
+            <button className={style.button} onClick={() => { localStorage.clear(); location.reload() }}>
+              <Octicon className={style.icon} name="sign-out" />
+            </button>
+          </Flex>
         }
       </Flex>
     )
